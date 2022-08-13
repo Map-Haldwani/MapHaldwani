99d01081810529e5694c0dc9ed521bc4c3a9f8d9
MG-LSJ <lakshyajeetjalal@gmail.com>
2022-08-13 11:06:51 +0530

    watergis fix

--- script.js
+++ script.js
@@ -70,11 +70,11 @@ map.addControl(
 if (!mobileCheck()) {
     map.addControl(new mapboxgl.FullscreenControl());
     map.addControl(
-        new watergis.MapboxExportControl({
-            PageSize: watergis.Size.A3,
-            PageOrientation: watergis.PageOrientation.Portrait,
-            Format: watergis.Format.PNG,
-            DPI: watergis.DPI[96],
+        new MapboxExportControl({
+            PageSize: Size.A4,
+            PageOrientation: PageOrientation.Portrait,
+            Format: Format.PNG,
+            DPI: DPI[96],
             Crosshair: true,
             PrintableArea: true,
         }),
